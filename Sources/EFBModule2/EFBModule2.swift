b0f32777dd6d88f6d3ca5be73f0f90d24dbc658f
Guillaume Ramey <guillaume.ramey@capgemini.com>
2022-01-10 16:57:25 +0100

    add view

--- Sources/EFBModule2/EFBModule2.swift
+++ Sources/EFBModule2/EFBModule2.swift
@@ -1,5 +1,7 @@
 public struct EFBModule2 {
-    public private(set) var title = "Module 2"
+  public private(set) var icon = "thermometer.sun"
+  public private(set) var title = "Weather"
+  public private(set) var subtitle = "Today's weather forecast."
 
     public init() {
     }
---
+++ Sources/EFBModule2/WeatherView.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherView.swift
+//  
+//
+//  Created by Guillaume Ramey on 10/01/2022.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    var body: some View {
+        Text("Weather View")
+    }
+}
+
+struct WeatherView_Previews: PreviewProvider {
+    static var previews: some View {
+      WeatherView()
+    }
+}
