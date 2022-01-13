754545584a4d7a115f7670bb0ed95c824b918970
Guillaume Ramey <guillaume.ramey@capgemini.com>
2022-01-13 11:36:20 +0100

    import EFBToolKit

--- Package.swift
+++ Package.swift
@@ -15,8 +15,7 @@ let package = Package(
             targets: ["EFBModule2"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+         .package(url: "https://github.com/guillaumerameycapg/EFBToolKit", .branch("main")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
--- Sources/EFBModule2/EFBModule2.swift
+++ Sources/EFBModule2/EFBModule2.swift
@@ -1,4 +1,6 @@
-public struct EFBModule2 {
+import EFBToolKit
+
+public struct EFBModule2: EFBModule {
   public private(set) var icon = "thermometer.sun"
   public private(set) var title = "Weather"
   public private(set) var subtitle = "Today's weather forecast."
